2131660ff9757e804317f0ae4b118de19b75f2c0
Ygor190305 <ygor95125771@gmail.com>
2021-05-08 21:21:15 -0300

    Finalizado

--- savior/duvidas-frequentes.php
+++ savior/duvidas-frequentes.php
@@ -34,8 +34,7 @@
                     <div class="list-group" id="list-tab" role="tablist">
                         <a class="list-group-item list-group-item-secondary list-group-item-action active" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="home">Links</a>
                         <a class="list-group-item list-group-item-secondary list-group-item-action" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Recuperação de Senhas</a>
-                        <a class="list-group-item list-group-item-secondary list-group-item-action" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Com quem falar</a>
-                        <a class="list-group-item list-group-item-secondary list-group-item-action" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Aplicativos</a>
+                        <a class="list-group-item list-group-item-secondary list-group-item-action" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Para quem abrir chamados</a>
                     </div>
                 </div>
                 <div class="col-8">
@@ -52,18 +51,50 @@
                             </div>  
                         </div>
                         <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
-                        
-                            <h3><b>Verdesys | Digitaliza | Verdecob</b></h3>
-                            <h3><b>CliQQ | VA | Gestor de Contatos | Painel do Colaborador | PDV</b></h3>
-                            <h3><b>Escola Virtual</b></h3>
-                            <h3><b>Totvs</b></h3>
-                            <h3><b>Workplace</b></h3>
-                            <h3><b>Helpdesk</b></h3>
-                            <h3><b>SAP</b></h3>
-                            <h3><b>Zimbra</b></h3>
+                            <div class="bg-secondary text-white shadow-lg rounded p-3">
+                                <h3><b>Verdesys | Digitaliza | Verdecob</b></h3>
+                                <h5 class="mx-5">Abrir um chamado para Verdecard Nível 2</h5>
+                                <h3><b>CliQQ | VA | Gestor de Contatos | Painel do Colaborador | PDV</b></h3>
+                                <h5 class="mx-5">Solicitar o reset ao Helpdesk</h5>
+                                <h3><b>Escola Virtual</b></h3>
+                                <h5 class="mx-5">Solicitar para a Denise Roedel</h5>
+                                <h3><b>Totvs</b></h3>
+                                <h5 class="mx-5">Solicitar ao Departamento Pessoal</h5>
+                                <h3><b>Workplace</b></h3>
+                                <h5 class="mx-5">Acessar o link 10.0.0.14:8000</h5>
+                                <h3><b>Helpdesk</b></h3>
+                                <h5 class="mx-5">Solicitar o reset ao Helpdesk</h5>
+                                <h3><b>SAP</b></h3>
+                                <h5 class="mx-5">Solicitar o reset ao Helpdesk</h5>
+                                <h3><b>Zimbra</b></h3>
+                                <h5 class="mx-5">Solicitar o reset ao Helpdesk</h5> 
+                            </div> 
+                            
                         </div>
-                        <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>
-                        <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">...</div>
+                        <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">
+                            <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
+                                <div class="bg-secondary text-white shadow-lg rounded p-3">
+                                    <h3><b>Problemas com abertura/fechamento de caixas</b></h3>
+                                    <h5 class="mx-5">Abrir um chamado para Tesouraria</h5>
+                                    <h3><b>Problemas com Gestor de Contatos | Painel do Colaborador | Rov | Etiquetas</b></h3>
+                                    <h5 class="mx-5">Abrir um chamado para Aplicações Web</h5>
+                                    <h3><b>Problemas com Equipamentos de Hardware</b></h3>
+                                    <h5 class="mx-5">Abrir um chamado para SOS Micros</h5>
+                                    <h3><b>Problemas com o CliQQ</b></h3>
+                                    <h5 class="mx-5">Abrir um chamado para CliQQ Sistemas</h5>
+                                    <h3><b>Problemas com o VA ou SAP</b></h3>
+                                    <h5 class="mx-5">Abrir um chamado para VA/SAP Sistemas</h5>
+                                    <h3><b>Problemas com Verdesys | Verdecob | Digitaliza</b></h3>
+                                    <h5 class="mx-5">Abrir um chamado para TI da Verde</h5>
+                                    <h3><b>Problemas com o cockpit e NFEs</b></h3>
+                                    <h5 class="mx-5">Abrir um chamado para Cockpit NFE</h5>
+                                    <h3><b>Problemas com o email Zimbra, proxy ou VPN para lojas</b></h3>
+                                    <h5 class="mx-5">Abrir um chamado para Infra</h5>
+                                    <h3><b>Problemas com VPN para matriz</b></h3>
+                                    <h5 class="mx-5">Abrir um chamado para Suporte Matriz</h5>  
+                                </div>         
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
